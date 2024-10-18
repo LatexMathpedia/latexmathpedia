5ca9068cf4a8d4ca3886e0817b317a7422b2476d
Pablo García <pablogarciapernas@gmail.com>
2024-10-18 20:14:53 +0200

    Credenciales escondidas, tengo que pasarte el .env

--- public/src/index.js
+++ public/src/index.js
@@ -1,11 +1,13 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import dotenv from 'dotenv';
+dotenv.config();
 const firebaseConfig = {
-  apiKey: "AIzaSyCBWnK5WsDsJw-byDy6FpPzh_lBOUIeicw",
-  authDomain: "mathtexpedia.firebaseapp.com",
-  projectId: "mathtexpedia",
-  storageBucket: "mathtexpedia.appspot.com",
-  messagingSenderId: "1051913187424",
-  appId: "1:1051913187424:web:e55eb4a9d779da3290d305",
-  measurementId: "G-4KF8ZJB6VD"
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId
 };
