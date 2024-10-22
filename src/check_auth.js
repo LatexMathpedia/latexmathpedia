159d4a55436b8fc95a422133e190d192ef055ebb
Pablo García <pablogarciapernas@gmail.com>
2024-10-22 23:00:49 +0200

    Arreglado script

--- src/check_auth.js
+++ src/check_auth.js
@@ -1,25 +1,41 @@
 import { auth, onAuthStateChanged } from "./firebase.js";
 
-onAuthStateChanged(auth, async (user) => {
+async function checkEmailVerification(user) {
+    await user.reload()
+    if (user.emailVerified) {
+        console.log("Email verificado");
+        window.location.href = 'index.html';
+    } else {
+        console.log("Email no verificado");
+    }
+}
+
+onAuthStateChanged(auth, (user) => {
     console.log("onAuthStateChanged detectado");
 
     if (user) {
-        await user.reload();
         const currentPage = window.location.pathname.split('/').pop();
 
         if (user.emailVerified) {
             console.log("Email verificado");
-
             if (currentPage === 'verificar_email.html') {
                 window.location.href = 'index.html';
             }
         } else {
             console.log("Email no verificado");
-
             if (currentPage !== 'verificar_email.html') {
                 window.location.href = 'verificar_email.html';
+            } else {
+                const interval = setInterval(async () => {
+                    await checkEmailVerification(user);
+                }, 3000);
+                window.addEventListener('beforeunload', () => {
+                    clearInterval(interval);
+                });
             }
         }
+    } else {
+        console.log("No hay usuario autenticado");
     }
 });
-setInterval(onAuthStateChanged, 1500)
+
