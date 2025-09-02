d2b2b30cf5d1cf21b82e941f83e1b5b71d3c054b
Pablo Garcia <pablogarciapernas@gmail.com>
2025-09-02 17:33:31 +0200

    Feat(Core): started router skeleton (need to have more pages to have it)

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,31 @@
 /* @refresh reload */
 import { render } from 'solid-js/web'
 import './index.css'
-import App from './App.jsx'
+import { Router } from '@solidjs/router'
+import { lazy } from 'solid-js'
 
 const root = document.getElementById('root')
 
-render(() => <App />, root)
+if (!root) throw new Error('Failed to find the root element')
+
+/* Cuando tengamos un header y un footer descomentar este Layout y añadir prop root = {Layout} en el Router
+    const Layout = (props) => {
+        return (
+            <>
+                <header>Header</header>
+                {props.children}
+                <footer>Footer</footer>
+            </>
+        )
+    }
+*/
+
+const routes = [
+    {
+        path: "/",
+        component: lazy(() => import("/pages/"))
+    }
+]
+
+render(() => <Router>{routes}</Router>
+    , root)
