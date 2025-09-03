ca9e0cf694e2fb74adfe8b9d37d4ad18bcb864a8
mendana <diegodiazmenda@gmail.com>
2025-09-03 19:57:34 +0200

    feat(site-header): agregar breadcrumbs dinámicos y mejorar la estructura del encabezado

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,6 +1,7 @@
 "use client"
 
-import { SidebarIcon } from "lucide-react"
+import { ChevronRight, SidebarIcon } from "lucide-react"
+import { usePathname } from "next/navigation"
 
 import { SearchForm } from "@/components/search-form"
 import { useSearch } from "@/contexts/search-context" // Importar el contexto de búsqueda
@@ -17,13 +18,21 @@ import { Separator } from "@/components/ui/separator"
 import { useSidebar } from "@/components/ui/sidebar"
 import { ModeToggle } from "./mode-toggle"
 
+function capitalizeFirstLetter(string: string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 export function SiteHeader() {
   const { toggleSidebar } = useSidebar()
-  const { setSearchQuery } = useSearch() 
+  const { setSearchQuery } = useSearch()
+  const pathname = usePathname() // Obtener la ruta actual
 
   const handleSearch = (query: string) => {
     setSearchQuery(query)
   }
+
+  // Dividir la ruta en segmentos para generar los breadcrumbs
+  const pathSegments = pathname.split("/").filter(Boolean)
 
   return (
     <header className="bg-background sticky top-0 z-50 flex w-full items-center border-b">
@@ -40,14 +49,26 @@ export function SiteHeader() {
         <Breadcrumb className="hidden sm:block">
           <BreadcrumbList>
             <BreadcrumbItem>
-              <BreadcrumbLink href="#">
-                MathTexpedia
-              </BreadcrumbLink>
+              <BreadcrumbLink href="/">MathTexpedia</BreadcrumbLink>
             </BreadcrumbItem>
-            <BreadcrumbSeparator />
-            <BreadcrumbItem>
-              <BreadcrumbPage>Página Principal</BreadcrumbPage>
-            </BreadcrumbItem>
+            <ChevronRight size={13}/>
+            {pathSegments.map((segment, index) => {
+              const href = `/${pathSegments.slice(0, index + 1).join("/")}`
+              const isLast = index === pathSegments.length - 1
+
+              return (
+                <BreadcrumbItem key={href}>
+                  {isLast ? (
+                    <BreadcrumbPage>{capitalizeFirstLetter(decodeURIComponent(segment))}</BreadcrumbPage>
+                  ) : (
+                    <BreadcrumbLink href={href}>
+                      {capitalizeFirstLetter(decodeURIComponent(segment))}
+                    </BreadcrumbLink>
+                  )}
+                  {!isLast && <BreadcrumbSeparator />}
+                </BreadcrumbItem>
+              )
+            })}
           </BreadcrumbList>
         </Breadcrumb>
         <SearchForm className="w-full sm:ml-auto sm:w-auto" onSearch={handleSearch} />
